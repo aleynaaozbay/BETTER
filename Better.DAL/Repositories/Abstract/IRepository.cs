87a88842adb3975b66b172d03905786f7e60ba3d
aleynaaozbay@gmail.com <aleynaaozbay@gmail.com>
2024-08-12 21:07:28 +0300

    Repo Basic

---
+++ Better.DAL/Repositories/Abstract/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Better.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Better.DAL.Repositories.Abstract
+{
+    public interface IRepository<T> where T : class, IEntityBase, new()
+    {
+        Task AddAsync(T entity);
+    }
+}
---
+++ Better.DAL/Repositories/Concrete/Repository.cs
@@ -0,0 +1,35 @@
+﻿using Better.Core.Entities;
+using Better.DAL.Context;
+using Better.DAL.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Better.DAL.Repositories.Concrete
+{
+    public class Repository<T>  :IRepository<T> where T: class, IEntityBase, new()
+    {
+        private readonly AppDbContext dbContext;
+
+        public Repository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        private DbSet<T> Table { get => dbContext.Set<T>(); }
+
+
+
+        public async Task AddAsync(T entity)
+        {
+           await Table.AddAsync(entity);
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null)
+    }
+}
+//Task = void 
