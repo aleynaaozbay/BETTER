5bdd064f161c59d56fe2b058eabd2d4667452e4e
aleynaaozbay@gmail.com <aleynaaozbay@gmail.com>
2024-08-18 20:47:15 +0300

    template

---
+++ Better.WebMVC/Recipes/RecipeHomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Better.WebMVC.Recipes
+{
+    public class RecipeHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
